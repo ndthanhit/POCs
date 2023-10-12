adf9fd18a4488b951cf65f9fcc0327b51d0d0c35
Nguyen Dang Thanh <thanh.nguyen@axon-networks.com>
2023-10-12 22:07:10 +0700

    Add maven shad plugin

--- graalvm-dynamic/src/main/java/dev/danvega/Application.java
+++ graalvm-dynamic/src/main/java/dev/danvega/Application.java
@@ -1,5 +1,7 @@
 package dev.danvega;
 
+import org.nd4j.common.primitives.Counter;
+
 import java.lang.reflect.InvocationTargetException;
 
 public class Application {
@@ -7,9 +9,27 @@ public class Application {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Class<?> niceClazz = Class.forName("dev.danvega.NiceMessage");
         niceClazz.getMethod("printMessage").invoke(niceClazz.getConstructor().newInstance());
-        Class<?> meanClazz = Class.forName("dev.danvega.MeanMessage");
-        meanClazz.getMethod("printMessage").invoke(meanClazz.getConstructor().newInstance());
-        Class<?> thanhClazz = Class.forName("dev.danvega.ThanhMessage");
-        thanhClazz.getMethod("printMessage").invoke(thanhClazz.getConstructor().newInstance());
+
+        Counter<String> counterA = new Counter<>();
+
+        counterA.incrementCount("A", 1);
+        counterA.incrementCount("A", 1);
+        counterA.incrementCount("A", 1);
+
+        Counter<String> counterB = new Counter<>();
+        counterB.incrementCount("B", 2);
+        counterB.incrementCount("B", 2);
+
+        System.out.println(counterA.getCount("A")); // 3
+        System.out.println(counterA.getCount("B")); // 4
+
+        counterA.incrementAll(counterB);
+
+        System.out.println(counterA.getCount("A")); // 3
+        System.out.println(counterA.getCount("B")); // 4
+
+        counterA.setCount("B", 234);
+
+        System.out.println(counterA.getCount("B")); // 234
     }
 }

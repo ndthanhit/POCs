d97e5a6dae04a9e1bcde664e7832b97949cc2d72
Nguyen Dang Thanh <thanh.nguyen@axon-networks.com>
2023-10-12 09:40:51 +0700

    Adding message

---
+++ graalvm-dynamic/src/main/java/dev/danvega/ThanhMessage.java
@@ -0,0 +1,10 @@
+package dev.danvega;
+
+public class ThanhMessage implements Message {
+
+    @Override
+    public void printMessage() {
+        System.out.println("This is a Thanh message!");
+    }
+
+}

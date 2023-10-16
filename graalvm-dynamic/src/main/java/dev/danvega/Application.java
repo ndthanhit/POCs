583f0625c0d64315d47dc3a094cd3621543eed4d
Nguyen Dang Thanh <thanh.nguyen@axon-networks.com>
2023-10-16 13:18:37 +0700

    Reverting code

--- graalvm-dynamic/src/main/java/dev/danvega/Application.java
+++ graalvm-dynamic/src/main/java/dev/danvega/Application.java
@@ -7,9 +7,10 @@ import java.lang.reflect.InvocationTargetException;
 public class Application {
 
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        /*Class<?> niceClazz = Class.forName("dev.danvega.NiceMessage");
+        Class<?> niceClazz = Class.forName("dev.danvega.NiceMessage");
         niceClazz.getMethod("printMessage").invoke(niceClazz.getConstructor().newInstance());
 
+        /*
         Class<?> meanClazz = Class.forName("dev.danvega.MeanMessage");
         meanClazz.getMethod("printMessage").invoke(niceClazz.getConstructor().newInstance());
 

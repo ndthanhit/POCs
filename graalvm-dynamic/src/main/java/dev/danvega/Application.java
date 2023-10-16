1330c8ca9c9595465ab20e2eb300b23a61f57c24
Nguyen Dang Thanh <thanh.nguyen@axon-networks.com>
2023-10-16 09:39:11 +0700

    Adding reflection to AtomicDouble

--- graalvm-dynamic/src/main/java/dev/danvega/Application.java
+++ graalvm-dynamic/src/main/java/dev/danvega/Application.java
@@ -1,35 +1,13 @@
 package dev.danvega;
 
-import org.nd4j.common.primitives.Counter;
-
 import java.lang.reflect.InvocationTargetException;
 
 public class Application {
 
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        Class<?> niceClazz = Class.forName("dev.danvega.NiceMessage");
-        niceClazz.getMethod("printMessage").invoke(niceClazz.getConstructor().newInstance());
+        Class<?> niceClazz = Class.forName("org.nd4j.shade.guava.util.concurrent.AtomicDouble");
+        System.out.println("Double value is: " + niceClazz.getMethod("longValue").invoke(niceClazz.getConstructor().newInstance()));
 
-        Counter<String> counterA = new Counter<>();
 
-        counterA.incrementCount("A", 1);
-        counterA.incrementCount("A", 1);
-        counterA.incrementCount("A", 1);
-
-        Counter<String> counterB = new Counter<>();
-        counterB.incrementCount("B", 2);
-        counterB.incrementCount("B", 2);
-
-        System.out.println(counterA.getCount("A")); // 3
-        System.out.println(counterA.getCount("B")); // 4
-
-        counterA.incrementAll(counterB);
-
-        System.out.println(counterA.getCount("A")); // 3
-        System.out.println(counterA.getCount("B")); // 4
-
-        counterA.setCount("B", 234);
-
-        System.out.println(counterA.getCount("B")); // 234
     }
 }
